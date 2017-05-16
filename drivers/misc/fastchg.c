f06e491e35dd82ea9103ff746a6e87627470ab91
tsubus <tsubus@ilwt.org>
2017-05-16 22:15:27 +0200

    Enable USB fast charge by default
    - There is no downside to having this always enabled
    
    Change-Id: Iff2473cef075b10199624e5dfd117fc3cf50cbd2

--- drivers/misc/fastchg.c
+++ drivers/misc/fastchg.c
@@ -17,8 +17,8 @@
 /*
  * Possible values for "force_fast_charge" are :
  *
- *   0 - Disabled (default)
- *   1 - Force faster charge
+ *   0 - Disabled
+ *   1 - Force faster charge (default)
 */
 
 #include <linux/kobject.h>
@@ -26,7 +26,7 @@
 #include <linux/fastchg.h>
 #include <linux/string.h>
 
-int force_fast_charge = 0;
+int force_fast_charge = 1;
 
 static int __init get_fastcharge_opt(char *ffc)
 {
@@ -35,7 +35,7 @@ static int __init get_fastcharge_opt(char *ffc)
 	} else if (strcmp(ffc, "1") == 0) {
 		force_fast_charge = 1;
 	} else {
-		force_fast_charge = 0;
+		force_fast_charge = 1;
 	}
 	return 1;
 }
@@ -53,7 +53,7 @@ static ssize_t force_fast_charge_store(struct kobject *kobj, struct kobj_attribu
 {
 	sscanf(buf, "%d ", &force_fast_charge);
 	if (force_fast_charge < 0 || force_fast_charge > 1)
-		force_fast_charge = 0;
+		force_fast_charge = 1;
 
 	return count;
 }
