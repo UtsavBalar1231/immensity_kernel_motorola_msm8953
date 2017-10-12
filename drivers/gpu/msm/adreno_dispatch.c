ed7c8318183d8a4440b59a4407a48bcbd3fb4b14
Kyle Piefer <kpiefer@codeaurora.org>
2017-10-12 16:20:36 -0700

    Revert "msm: kgsl: Disallow L2PC during wake up from SLUMBER"
    This reverts commit 5344e5c78f5820dfa34cfcea4572d8e347a018ce.
    The change negatively impacted performance.
    
    CRs-Fixed: 2120475
    Change-Id: Ib6ff329a3501e77d990c2e9502ed35e041f730c8
    Signed-off-by: Kyle Piefer <kpiefer@codeaurora.org>
    Signed-off-by: Alex Naidis <alex.naidis@paranoidandroid.co>
    Signed-off-by: utsavbalar1231 <utsavbalar1231@gmail.com>

--- drivers/gpu/msm/adreno_dispatch.c
+++ drivers/gpu/msm/adreno_dispatch.c
@@ -1192,9 +1192,7 @@ int adreno_dispatcher_queue_cmd(struct adreno_device *adreno_dev,
 
 	spin_unlock(&drawctxt->lock);
 
-	if (device->pwrctrl.l2pc_update_queue)
-		kgsl_pwrctrl_update_l2pc(&adreno_dev->dev,
-				KGSL_L2PC_QUEUE_TIMEOUT);
+	kgsl_pwrctrl_update_l2pc(&adreno_dev->dev);
 
 	/* Add the context to the dispatcher pending list */
 	dispatcher_queue_context(adreno_dev, drawctxt);
