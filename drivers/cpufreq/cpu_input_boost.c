8f9f1d092683c391459cac1c97b718caf48f208d
Francisco Franco <franciscofranco.1990@gmail.com>
2018-06-26 03:37:46 +0000

    cpu_input_boost: expose input freqs and duration to userspace
    Users like to change stuff and it's useful to tie it in with my
    Performance Profiles and it's faster to test new configurations
    than recompiling the whole thing and flash every time.
    
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: utsavbalar1231 <utsavbalar1231@gmail.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -9,7 +9,14 @@
 #include <linux/cpufreq.h>
 #include <linux/fb.h>
 #include <linux/input.h>
+#include <linux/moduleparam.h>
 #include <linux/slab.h>
+
+static unsigned int input_boost_freq = CONFIG_INPUT_BOOST_FREQ;
+static unsigned short input_boost_duration_ms = CONFIG_INPUT_BOOST_DURATION_MS;
+
+module_param(input_boost_freq, uint, 0644);
+module_param(input_boost_duration_ms, short, 0644);
 
 /* Available bits for boost_drv state */
 #define SCREEN_AWAKE		BIT(0)
@@ -117,7 +124,7 @@ static void input_boost_worker(struct work_struct *work)
 	}
 
 	queue_delayed_work(b->wq, &b->input_unboost,
-		msecs_to_jiffies(CONFIG_INPUT_BOOST_DURATION_MS));
+		msecs_to_jiffies(input_boost_duration_ms));
 }
 
 static void input_unboost_worker(struct work_struct *work)
@@ -174,7 +181,7 @@ static int cpu_notifier_cb(struct notifier_block *nb,
 	 * unboosting, set policy->min to the absolute min freq for the CPU.
 	 */
 	if (state & INPUT_BOOST) {
-		policy->min = min(policy->max, (unsigned int)CONFIG_INPUT_BOOST_FREQ);
+		policy->min = min(policy->max, (unsigned int)input_boost_freq);
 	} else {
 		policy->min = policy->cpuinfo.min_freq;
 	}
