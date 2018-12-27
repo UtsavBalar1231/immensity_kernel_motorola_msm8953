5490859aeff0d6bc6b9745ee82c2e89f6d1c2dfd
utsavbalar1231 <utsavbalar1231@gmail.com>
2018-12-27 17:08:30 +0000

    update build script

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 #set -e
@@ -6,13 +7,14 @@ DATE_POSTFIX=$(date +"%Y%m%d")
 
 ## Copy this script inside the kernel directory
 KERNEL_DIR=$PWD
-KERNEL_TOOLCHAIN=/home/nick/data/kernel/prebuilts/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-CLANG_TOOLCHAIN=/home/nick/data/kernel/prebuilts/clang-r346389b/bin/clang-8
+KERNEL_TOOLCHAIN=/home/jonsnowkinginthenorth23/ros/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+CLANG_TOOLCHAIN=/home/jonsnowkinginthenorth23/ros/prebuilts/clang/host/linux-x86/7.0-DragonTC/bin/clang-7
 KERNEL_DEFCONFIG=potter_defconfig
 DTBTOOL=$KERNEL_DIR/Dtbtool/
+OUT=/home/ros/out
 JOBS=28
 ANY_KERNEL2_DIR=$KERNEL_DIR/AnyKernel2/
-FINAL_KERNEL_ZIP=Optimus_Drunk_Potter-$DATE_POSTFIX-EAS.zip
+FINAL_KERNEL_ZIP=IMMENSITY_v1.2-EAS_byTheCUNT-$DATE_POSTFIX-EAS.zip
 # Speed up build process
 MAKE="./makeparallel"
 
@@ -27,7 +29,7 @@ echo "**** Setting Toolchain ****"
 export CROSS_COMPILE=$KERNEL_TOOLCHAIN
 export ARCH=arm64
 export SUBARCH=arm64
-export KBUILD_COMPILER_STRING="Clang Version 8.0.6"
+export KBUILD_COMPILER_STRING="Clang Version 7.0-DragonTC"
 
 # Clean build always lol
 echo "**** Cleaning ****"
@@ -65,15 +67,10 @@ cp $KERNEL_DIR/out/arch/arm64/boot/dtb $ANY_KERNEL2_DIR/
 echo "**** Time to zip up! ****"
 cd $ANY_KERNEL2_DIR/
 zip -r9 $FINAL_KERNEL_ZIP * -x README $FINAL_KERNEL_ZIP
-cp $KERNEL_DIR/AnyKernel2/$FINAL_KERNEL_ZIP /home/nick/data/kernel/$FINAL_KERNEL_ZIP
+cp $KERNEL_DIR/AnyKernel2/$FINAL_KERNEL_ZIP $KERNEL_DIR/$FINAL_KERNEL_ZIP
 
 echo "**** Good Bye!! ****"
 cd $KERNEL_DIR
-rm -rf arch/arm64/boot/dtb
-rm -rf $ANY_KERNEL2_DIR/$FINAL_KERNEL_ZIP
-rm -rf AnyKernel2/Image.gz
-rm -rf AnyKernel2/dtb
-rm -rf $KERNEL_DIR/out/
 
 BUILD_END=$(date +"%s")
 DIFF=$(($BUILD_END - $BUILD_START))
