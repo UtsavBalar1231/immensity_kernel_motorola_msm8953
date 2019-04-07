6c6ba5465a0759b082f2da5cd4491c3f54d8d152
utsavbalar1231 <utsavbalar1231@gmail.com>
2019-04-07 13:22:42 +0000

    zram: increase max compression streams to 4
    Signed-off-by: utsavbalar1231 <utsavbalar1231@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1320,7 +1320,7 @@ static int zram_add(void)
 
 	strlcpy(zram->compressor, default_compressor, sizeof(zram->compressor));
 	zram->meta = NULL;
-	zram->max_comp_streams = 1;
+	zram->max_comp_streams = 4;
 
 	pr_info("Added device: %s\n", zram->disk->disk_name);
 	return device_id;
