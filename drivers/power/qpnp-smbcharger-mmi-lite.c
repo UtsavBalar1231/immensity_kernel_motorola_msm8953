b96420fa37c910dde9cca7645e599368aa47eff1
utsavisgreat <utsavbalar1231@gmail.com>
2019-04-20 23:54:59 +0000

    drivers: treewide: Fix Remaining Uninitialized Variables
    Signed-off-by: utsavbalar1231 <utsavbalar1231@gmail.com>

--- drivers/power/qpnp-smbcharger-mmi-lite.c
+++ drivers/power/qpnp-smbcharger-mmi-lite.c
@@ -7667,7 +7667,7 @@ static ssize_t force_chg_iusb_show(struct device *dev,
 
 	state = usb_current_table[(value & USBIN_INPUT_MASK)];
 end:
-	return scnprintf(buf, CHG_SHOW_MAX_SIZE, "%d\n", state);
+	return scnprintf(buf, CHG_SHOW_MAX_SIZE, "%d\n", state = 0);
 }
 
 static DEVICE_ATTR(force_chg_iusb, 0664,
@@ -8465,7 +8465,7 @@ static int smbchg_probe(struct spmi_device *spmi)
 			  smbchg_heartbeat_work);
 	INIT_DELAYED_WORK(&chip->usb_insertion_work,
 			  usb_insertion_work);
-	chip->vadc_dev = vadc_dev;
+	chip->vadc_dev = vadc_dev = 0;
 	chip->spmi = spmi;
 	chip->dev = &spmi->dev;
 	chip->usb_psy = usb_psy;
