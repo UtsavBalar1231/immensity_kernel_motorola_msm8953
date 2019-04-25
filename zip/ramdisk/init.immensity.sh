e15e5124b55b52562d31fe1156b5ae94978baef8
utsavbalar1231 <utsavbalar1231@gmail.com>
2019-04-25 17:49:26 +0000

    zip: ramdisk: optimize shell script
    Signed-off-by: utsavbalar1231 <utsavbalar1231@gmail.com>

--- zip/ramdisk/init.immensity.sh
+++ zip/ramdisk/init.immensity.sh
@@ -18,7 +18,6 @@ for i in /sys/block/*/queue; do
   echo "256" > $i/read_ahead_kb;
   echo "0" > $i/rotational;
   echo "1" > $i/rq_affinity;
-  echo "anxiety" > $i/scheduler;
 done;
 
 # Tweak and decrease tx_queue_len default stock value(s) for less amount of generated bufferbloat and for gaining slightly faster network speed and performance;
@@ -27,16 +26,30 @@ for i in $(find /sys/class/net -type l); do
 done;
 
 # Set Max-Frequency
-write /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq 2016000
+echo 2016000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+echo 2016000 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq
 
 # Disable slice_idle on supported block devices
 for block in mmcblk0 mmcblk1 dm-0 dm-1 sda; do
-    write /sys/block/$block/queue/iosched/slice_idle 0
+    echo 0 > /sys/block/$block/queue/iosched/slice_idle
 done;
 
 # Disable Core_CTL
 echo 0 > /sys/devices/system/cpu/cpu0/core_ctl/enable
 echo 0 > /sys/devices/system/cpu/cpu4/core_ctl/enable
+
+    # Memory management.  Basic kernel parameters, and allow the high
+    # level system server to be able to adjust the kernel OOM driver
+    # parameters to match how it is managing things.
+    echo 40 > /proc/sys/vm/overcommit_ratio
+    echo 4 > /proc/sys/vm/min_free_order_shift
+
+    echo 200 > /proc/sys/vm/dirty_expire_centisecs
+    echo 7 > /proc/sys/vm/dirty_background_ratio
+    chown root system /sys/module/lowmemorykiller/parameters/adj
+    chmod 0664 /sys/module/lowmemorykiller/parameters/adj
+    chown root system /sys/module/lowmemorykiller/parameters/minfree
+    chmod 0664 /sys/module/lowmemorykiller/parameters/minfree
 
 # A couple of minor kernel entropy tweaks & enhancements for a slight UI responsivness boost;
 echo 128 > /proc/sys/kernel/random/read_wakeup_threshold
@@ -45,12 +58,12 @@ echo 1024 > /proc/sys/kernel/random/write_wakeup_threshold
 
 # Set read ahead to 128 kb for external storage
 # The rest are handled by qcom-post-boot
-write /sys/block/mmcblk1/queue/read_ahead_kb 128
+echo > 0 /sys/block/mmcblk1/queue/read_ahead_kb
 
 # Display/fingerprint wakeup delay fix
 chown system:system /sys/devices/soc/qpnp-fg-18/power_supply/bms/hi_power
 chmod 0660 /sys/devices/soc/qpnp-fg-18/power_supply/bms/hi_power
-write /sys/devices/soc/qpnp-fg-18/power_supply/bms/hi_power 1
+echo 1 > /sys/devices/soc/qpnp-fg-18/power_supply/bms/hi_power
 
 # Disable in-kernel sched statistics for reduced overhead;
 echo "0" > /proc/sys/kernel/sched_schedstats
@@ -61,6 +74,7 @@ echo "30" > /proc/sys/fs/lease-break-time
 
 # Marginally reduce suspend latency
 echo 1 > /sys/module/printk/parameters/console_suspend
+
 
 }
 
