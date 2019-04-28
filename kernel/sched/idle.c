52ab1d529a6ea26186037e34853e1d7744876c25
utsavbalar1231 <utsavbalar1231@gmail.com>
2019-04-28 15:01:45 +0000

    Revert "BACKPORT: sched/idle: Optimize the generic idle loop"
    This reverts commit a48415151fe1a986c0b51987b54efc4c8c98bf54.
    
    Signed-off-by: utsavbalar1231 <utsavbalar1231@gmail.com>

--- kernel/sched/idle.c
+++ kernel/sched/idle.c
@@ -231,8 +231,6 @@ DEFINE_PER_CPU(bool, cpu_dead_idle);
  */
 static void cpu_idle_loop(void)
 {
-	int cpu = smp_processor_id();
-
 	while (1) {
 		/*
 		 * If the arch has a polling bit, we maintain an invariant:
@@ -251,7 +249,7 @@ static void cpu_idle_loop(void)
 			check_pgt_cache();
 			rmb();
 
-			if (cpu_is_offline(cpu)) {
+			if (cpu_is_offline(smp_processor_id())) {
 				smp_mb(); /* all activity before dead. */
 				this_cpu_write(cpu_dead_idle, true);
 				arch_cpu_idle_dead();
